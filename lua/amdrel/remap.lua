183141bfac39e563c0cac5403af5d79d3b0bd328
Jamie Kuppens <jamie.kuppens@thisdot.co>
2024-06-02 14:19:11 -0700

    Porting over some of my old configs

--- lua/amdrel/remap.lua
+++ lua/amdrel/remap.lua
@@ -26,11 +26,6 @@ vim.keymap.set("n", "Q", "<nop>")
 -- vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
 vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
 
-vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
-vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
-vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
-vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
-
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
@@ -43,8 +38,16 @@ vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 -- )
 
 vim.keymap.set("n", "<leader>vpp", "<cmd>e ~/.config/nvim/lua/amdrel/packer.lua<CR>");
--- vim.keymap.set("n", "<leader>mr", "<cmd>CellularAutomaton make_it_rain<CR>");
+vim.keymap.set("n", "<leader>mr", "<cmd>CellularAutomaton make_it_rain<CR>");
 
 vim.keymap.set("n", "<leader><leader>", function()
     vim.cmd("so")
 end)
+
+-- I'm lazy and don't like pressing <C-w>, but also I like my wrist being healthy
+vim.keymap.set("n", "<C-h>", "<cmd>wincmd h<CR>")
+vim.keymap.set("n", "<C-j>", "<cmd>wincmd j<CR>")
+vim.keymap.set("n", "<C-k>", "<cmd>wincmd k<CR>")
+vim.keymap.set("n", "<C-l>", "<cmd>wincmd l<CR>")
+
+vim.keymap.set("t", "<C-X>", "<C-\\><C-n>")
--- lua/amdrel/set.lua
+++ lua/amdrel/set.lua
@@ -1,14 +1,14 @@
 vim.opt.nu = true
 vim.opt.relativenumber = true
+vim.opt.cursorline = true
+vim.opt.colorcolumn = "80,100,120"
+vim.opt.wrap = false
 
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
-
 vim.opt.smartindent = true
-
-vim.opt.wrap = false
 
 vim.opt.swapfile = false
 vim.opt.backup = false
@@ -18,12 +18,35 @@ vim.opt.undofile = true
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
 
-vim.opt.termguicolors = true
-
 vim.opt.scrolloff = 8
 vim.opt.signcolumn = "yes"
 vim.opt.isfname:append("@-@")
 
+vim.opt.termguicolors = true
 vim.opt.updatetime = 50
 
-vim.opt.colorcolumn = "80,100,120"
+-- System clipboard integration
+if vim.fn.has("unix") == 1 then
+    local os = vim.fn.system("uname"):gsub("\n", "")
+
+    if os == "Darwin" then
+        vim.opt.clipboard = "unnamed"
+    elseif os == "Linux" or os == "FreeBSD" or os == "OpenBSD" or os == "NetBSD" then
+        vim.opt.clipboard = "unnamedplus"
+    end
+elseif vim.fn.executable("win32yank.exe") == 1 then
+  vim.opt.clipboard = "unnamedplus"
+
+  vim.g.clipboard = {
+      name = "win32yank";
+      copy = {
+        ["+"] = "win32yank.exe -i --crlf",
+        ["*"] = "win32yank.exe -i --crlf",
+      },
+      paste = {
+        ["+"] = "win32yank.exe -o --lf",
+        ["*"] = "win32yank.exe -o --lf",
+      },
+      cache_enabled = 0,
+  }
+end
