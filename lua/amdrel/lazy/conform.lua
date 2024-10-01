d5e9992d8e71dd80268aaf5d2eca8c6fc9c27833
Jamie Kuppens <reshurum@gmail.com>
2024-10-01 07:59:46 -0700

    Add auto-formatter, buffer cleanup and auto tagging

--- lua/amdrel/lazy/conform.lua
+++ lua/amdrel/lazy/conform.lua
@@ -1,21 +1,29 @@
 return {
-    "stevearc/conform.nvim",
-    opts = {
-        formatters_by_ft = {
-            javascript = { "prettier" },
-            lua = { "stylua" },
-            python = { "isort", "black" },
-            rust = { "rustfmt", lsp_format = "fallback" },
-            php = { "pint" },
-        },
-        format_on_save = {
-            -- These options will be passed to conform.format()
-            timeout_ms = 500,
-            lsp_format = "fallback",
-        },
-    },
+	"stevearc/conform.nvim",
+	opts = {
+		formatters_by_ft = {
+			javascript = { "prettier" },
+			typescript = { "prettier" },
+			html = { "prettier" },
+			css = { "prettier" },
+			scss = { "prettier" },
+			json = { "prettier" },
+			vue = { "prettier" },
+			jsx = { "prettier" },
+			tsx = { "prettier" },
+			lua = { "stylua" },
+			python = { "isort", "black" },
+			rust = { "rustfmt", lsp_format = "fallback" },
+			php = { "pint" },
+		},
+		format_on_save = {
+			-- These options will be passed to conform.format()
+			timeout_ms = 500,
+			lsp_format = "fallback",
+		},
+	},
 
-    config = function(_, opts)
-        require("conform").setup(opts)
-    end
+	config = function(_, opts)
+		require("conform").setup(opts)
+	end,
 }
