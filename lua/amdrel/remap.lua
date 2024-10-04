aaba7c5c69e0bf30baf752cf5a917d3a254b886e
Jamie Kuppens <reshurum@gmail.com>
2024-10-04 11:26:04 -0700

    Add LSP diagnostic keybind

--- lua/amdrel/remap.lua
+++ lua/amdrel/remap.lua
@@ -4,6 +4,10 @@ vim.keymap.set("n", "<leader>pv", function()
 	oil.open(oil.get_current_dir())
 end)
 
+vim.keymap.set("n", "<leader>e", function()
+	vim.diagnostic.open_float()
+end)
+
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
